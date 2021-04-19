dab06e217ad19202c3959ccdcdeb6658d02f619e
AJ-Romera <ajromeraalfonso@gmail.com>
2021-04-19 17:38:12 +0200

    Primer commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+function Home() {
+    return (
+        <div className={styles.container}>
+            <h1>Inicio</h1>
+        </div>
+    );
+}
+
+export default Home;
