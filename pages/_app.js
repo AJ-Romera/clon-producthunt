372aead95664f0ab01e8c56abce4ba84211d498d
AJ-Romera <ajromeraalfonso@gmail.com>
2021-04-22 18:35:39 +0200

    CHNG: Crea el Context de Firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import App from 'next/app';
+import '../styles/globals.css';
+import firebase, { FirebaseContext } from '../firebase';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <FirebaseContext.Provider value={{ firebase }}>
+            <Component {...pageProps} />
+        </FirebaseContext.Provider>
+    );
 }
 
-export default MyApp
+export default MyApp;
