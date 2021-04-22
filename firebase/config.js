c99a7f2f93c3cb1d3b59a0e08b6e3f8334521987
AJ-Romera <ajromeraalfonso@gmail.com>
2021-04-22 17:45:02 +0200

    CHNG: Configuración inicial Firebase

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: 'clon-phunt.firebaseapp.com',
+    projectId: 'clon-phunt',
+    storageBucket: 'clon-phunt.appspot.com',
+    messagingSenderId: '395219007729',
+    appId: '1:395219007729:web:4e75d965878ecef7a25150',
+};
+
+export default firebaseConfig;
