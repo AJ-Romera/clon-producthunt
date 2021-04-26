bb989e4b65a2d975f624480d243694cfb2b6b617
AJ-Romera <ajromeraalfonso@gmail.com>
2021-04-26 19:19:40 +0200

    CHNG: Hace Query para Traer la Lista de Productos

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import Layout from '../components/layout/Layout';
+import { FirebaseContext } from '../firebase';
 
 function Home() {
+    const [productos, setProductos] = useState([]);
+
+    const { firebase } = useContext(FirebaseContext);
+
+    useEffect(() => {
+        const obtenerProductos = () => {
+            firebase.db
+                .collection('productos')
+                .orderBy('creado', 'desc')
+                .onSnapshot(handleSnapshot);
+        };
+        obtenerProductos();
+    }, []);
+
+    function handleSnapshot(snapshot) {
+        const productos = snapshot.docs.map((doc) => {
+            return {
+                id: doc.id,
+                ...doc.data(),
+            };
+        });
+
+        setProductos(productos);
+    }
+
     return (
         <Layout>
             <h1>Inicio</h1>
