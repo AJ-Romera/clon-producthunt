8a425a8a8c61445b76a880fc876a756c001ac5bd
AJ-Romera <ajromeraalfonso@gmail.com>
2021-04-27 14:18:07 +0200

    CHNG: Muestra Info de los Productos desde la BD

--- components/layout/DetallesProducto.js
+++ components/layout/DetallesProducto.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import { es } from 'date-fns/locale';
+
+const Imagen = styled.img`
+    width: 200px;
+`;
 
 function DetallesProducto({ producto }) {
     const {
@@ -16,14 +23,38 @@ function DetallesProducto({ producto }) {
     return (
         <li>
             <div>
-                <div></div>
+                <div>
+                    <Imagen src={urlImagen} alt={`Imagen de ${nombre}`} />
+                </div>
 
                 <div>
                     <h1>{nombre}</h1>
+
+                    <p>{descripcion}</p>
+
+                    <div>
+                        <img
+                            src='/static/img/comentario.png'
+                            alt='Imagen de comentarios'
+                        />
+                        {comentarios.length === 0 ? (
+                            <p>No hay comentarios, sé el primero en comentar</p>
+                        ) : (
+                            <p>{comentarios.length} comentarios</p>
+                        )}
+                    </div>
+
+                    <p>
+                        Publicado hace:{' '}
+                        {formatDistanceToNow(new Date(creado), { locale: es })}
+                    </p>
                 </div>
             </div>
 
-            <div></div>
+            <div>
+                <div> &#9650; </div>
+                <p>{votos}</p>
+            </div>
         </li>
     );
 }
