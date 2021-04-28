11e8769d1cc464e1169edd8110be7b4fc8db4776
AJ-Romera <ajromeraalfonso@gmail.com>
2021-04-28 12:56:54 +0200

    CHNG: Estructura el Producto en dos Columnas

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -1,10 +1,21 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { useRouter } from 'next/router';
 
-import { FirebaseContext } from '../../firebase';
+import styled from '@emotion/styled';
+import { css, jsx } from '@emotion/react';
 
 import Error404 from '../../components/layout/404';
 import Layout from '../../components/layout/Layout';
+
+import { FirebaseContext } from '../../firebase';
+
+const ContenedorProducto = styled.div`
+    @media (min-width: 768px) {
+        display: grid;
+        grid-template-columns: 2fr 1fr;
+        column-gap: 2rem;
+    }
+`;
 
 function Producto() {
     // State del componente
@@ -37,9 +48,41 @@ function Producto() {
         }
     }, [id]);
 
+    if (Object.keys(producto).length === 0) return 'Cargando...';
+
+    const {
+        comentarios,
+        creado,
+        descripcion,
+        empresa,
+        nombre,
+        url,
+        urlImagen,
+        votos,
+    } = producto;
+
     return (
         <Layout>
-            <>{error && <Error404 />}</>
+            <>
+                {error && <Error404 />}
+
+                <div className='contenedor'>
+                    <h1
+                        css={css`
+                            margin-top: 5rem;
+                            text-align: center;
+                        `}
+                    >
+                        {nombre}
+                    </h1>
+
+                    <ContenedorProducto>
+                        <div>1</div>
+
+                        <aside>2</aside>
+                    </ContenedorProducto>
+                </div>
+            </>
         </Layout>
     );
 }
